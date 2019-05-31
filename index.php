fbf7083416cd469e37daf0927a39234ec043aa75
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-05-31 16:01:26 -0300

    Implementando router.

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-echo("Hello world!")
+require_once('routing/Router.php');
+
+Router::get('/', function($request) {
+    echo('home');
+});
+
+Router::get('/doctors', function($request) {
+    echo('doctors');
+});
+
+Router::get('/doctors/3/details', function($request) {
+    echo('doctors');
+});
+
+phpinfo();
+
+Router::run('/is2med-api');
 
 ?>
--- routing/Router.php
+++ routing/Router.php
@@ -1,16 +1,62 @@
 <?php
 
 class Router {
-    private static $instance;
-    private $routes = array();
-    private function __construct() {}
-    private function __clone() {}
-    private function __wakeup() {}
-    public function getInstance() {
-        if (self::$instance === null) {
-            self::$instance = new self;
+    private static $routes = array(
+        'GET' => array(),
+        'POST' => array(),
+        'PUT' => array(),
+        'PATCH' => array(),
+        'DELETE' => array()
+    );
+    private static $notFoundFunction;
+    private static $methodNotAllowedFunction;
+    private static function add($method, $pattern, $function) {
+        $pathArray = explode('/', $pattern);
+        $pathArray = array_filter($pathArray, function($s) { return $s != ''; });
+        $pathArray = array_map(function($s) { return '('.$s.')'; }, $pathArray);
+        $pattern = implode('/', $pathArray);
+        if ($pattern == '/') {
+            $pattern = '^[/]?$';
+        } else {
+            $pattern = '^/'.$pattern.'[/]?';
         }
-        return self::$instance;
+        self::$routes[$method][$pattern] = $function;
+    }
+    public static function get($pattern, $function) {
+        self::add('GET', $pattern, $function);
+    }
+    public static function post($pattern, $function) {
+        self::add('POST', $pattern, $function);
+    }
+    public static function put($pattern, $function) {
+        self::add('PUT', $pattern, $function);
+    }
+    public static function patch($pattern, $function) {
+        self::add('PATCH', $pattern, $function);
+    }
+    public static function delete($pattern, $function) {
+        self::add('DELETE', $pattern, $function);
+    }
+    public static function notFound($function) {
+        self::$notFoundFunction = $function;
+    }
+    public static function methodNotAllowed($function) {
+        self::$methodNotAllowedFunction = $function;
+    }
+    public static function run($basepath) {
+        $parsedURL = parse_url($_SERVER['REQUEST_URI']);
+        if(isset($parsed_url['path'])){
+            $path = $parsed_url['path'];
+            $path = preg_replace('^/'.preg_quote($basepath), '', $path);
+        } else {
+            $path = '/';
+        }
+        $method = $_SERVER['REQUEST_METHOD'];
+        echo($path);
+        foreach (self::$routes[$method] as $pattern => $function) {
+            print_r($pattern);
+            echo('<br/>');
+        }
     }
 }
 
