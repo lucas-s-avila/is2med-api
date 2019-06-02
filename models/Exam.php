c6df65a4f7584b416bef08a046855bcf2296a45e
lucas-s-avila <lucas.soares.avila@gmail.com>
2019-06-02 14:27:11 -0300

    finished data structs

---
+++ models/Exam.php
@@ -0,0 +1,67 @@
+<?php
+require_once("Doctor.php");
+require_once("Pacient.php");
+
+class Exam {
+    private $id;
+    private $date;
+    private $lab;
+    private $pacient;
+    private $type;
+    private $result;
+
+    function __construct($id, $date, $lab, $pacient, $type, $result) {
+        $this->id = $id;
+        $this->date = $date;
+        $this->lab = $lab;
+        $this->pacient = $pacient;
+        $this->type = $type;
+        $this->result = $result;
+    }
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getDate() {
+        return $this->date;
+    }
+
+    function setDate($date) {
+        $this->date = $date;
+    }
+
+    function getLab() {
+        return $this->lab;
+    }
+
+    function setLab($lab) {
+        $this->lab = $lab;
+    }
+
+    function getPacient() {
+        return $this->pacient;
+    }
+
+    function setPacient($pacient) {
+        $this->pacient = $pacient;
+    }
+
+    function getType() {
+        return $this->type;
+    }
+
+    function setType($type) {
+        $this->type = $type;
+    }
+
+    function getResult() {
+        return $this->result;
+    }
+
+    function setResult($result) {
+        $this->result = $result;
+    }
+}
+
+?>
